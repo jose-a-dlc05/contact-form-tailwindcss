0bb01e69fa9126b7e83f19765af2e18cf6b1b384
Jose De La Cruz <jose.delcruz0490@icloud.com>
2024-08-25 15:18:40 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	theme: {
+		extend: {
+			fontFamily: {
+				karla: ['Karla', 'sans-serif'],
+			},
+			colors: {
+				primary: {
+					lighter: '#dff1e7',
+					medium: '#0c7d69',
+					red: '#d73c3c',
+				},
+				neutral: {
+					medium: '#87a3a6',
+					dark: '#2b4246',
+				},
+			},
+		},
+	},
+	plugins: [],
+};
